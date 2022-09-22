77e693a61a8be826fbbb89bc4f5b4f342ad14915
Paul <paul@theapplegates.com>
2022-09-22 19:04:22 -0400

    Update layout-responsive.tsx

--- pages/layout-responsive.tsx
+++ pages/layout-responsive.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import ViewSource from '../components/view-source'
 import mountains from '../public/mountains.jpg'
 import g400 from '../public/G400-1.jpg'
+import cat from '../public/cat.jpg'
 
 const Responsive = () => (
   <div>
@@ -9,7 +10,7 @@ const Responsive = () => (
     <h1>Image Component With Layout Responsive</h1>
     <Image
       alt="Mountains"
-      src={g400}
+      src={cat}
       layout="responsive"
       width={700}
       height={475}
